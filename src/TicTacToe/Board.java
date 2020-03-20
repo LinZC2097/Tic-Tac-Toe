9b7bd4b5fecb394d9e11a6429c0af18744b2cad2
LinZC2097 <linzhichao_2813@163.com>
2020-03-20 00:27:00 -0400

    update class variable: Board.availablemove

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -1,6 +1,8 @@
 package TicTacToe;
 
-import java.lang.Math;;
+import java.lang.Math;
+import java.util.HashSet;
+import java.util.Hashtable;;
 
 public class Board {
 	static final int BOARD_LENGTH = 15;
@@ -10,21 +12,58 @@ public class Board {
 		X, O, Blank
 	};
 
-	public State[][] board;
-	public int moveNum;
+	private State[][] board;
+	private HashSet<Integer> availableMove;
+	private int moveNum;
+	private State player;
 
-	Board(){
+	Board() {
 		this.board = new State[BOARD_LENGTH][BOARD_LENGTH];
 		this.moveNum = 0;
+		this.player = State.O;
+		this.availableMove = new HashSet<>();
 	}
-	
+
 	public void initialization() {
-		
+
 		for (int i = 0; i < BOARD_LENGTH; i++) {
 			for (int j = 0; j < BOARD_LENGTH; j++) {
 				this.board[i][j] = State.Blank;
 			}
 		}
+
+		for (int i = 0; i < BOARD_LENGTH * BOARD_LENGTH; i++) {
+			this.availableMove.add(i);
+		}
+	}
+
+	public Board deepcopy() {
+		Board newBoard = new Board();
+
+		newBoard.initialization();
+
+		for (int i = 0; i < Board.BOARD_LENGTH; i++) {
+			newBoard.board[i] = this.board[i].clone();
+		}
+
+		newBoard.availableMove = (HashSet) this.availableMove.clone();
+
+		newBoard.moveNum = this.moveNum;
+		newBoard.player = this.player;
+
+		return newBoard;
+	}
+
+	public int getAvaliableMoveLen() {
+		return this.availableMove.size();
+	}
+
+	public HashSet<Integer> getAvailableMove() {
+		return this.availableMove;
+	}
+
+	public int[] getMoveIndex(int moveIndex) {
+		return new int[] { (moveIndex / BOARD_LENGTH), (moveIndex % BOARD_LENGTH) };
 	}
 
 	public boolean isUseless(int x, int y) {
@@ -70,7 +109,7 @@ public class Board {
 
 		return false;
 	}
-	
+
 	public void move(int x, int y) {
 		State player;
 		if (this.moveNum % 2 == 0) {
@@ -78,39 +117,51 @@ public class Board {
 		} else {
 			player = State.X;
 		}
-		
+
 		this.board[x][y] = player;
+		this.availableMove.remove(x * BOARD_LENGTH + y);
 		this.moveNum++;
+		this.player = this.player == State.O ? State.X : State.O;
 	}
-	
+
 	public void show() {
-		System.out.print(" \t");
-		for(int i = 0; i < 10; i++) {
+		System.out.print("x    y \t");
+
+		for (int i = 0; i < 10; i++) {
 			System.out.print(i + " ");
 		}
-		for(int i = 10; i < BOARD_LENGTH; i++) {
+		for (int i = 10; i < BOARD_LENGTH; i++) {
 			System.out.print(i);
 		}
 		System.out.println();
-		for(int i = 0; i < BOARD_LENGTH; i++) {
+		for (int i = 0; i < BOARD_LENGTH; i++) {
 			System.out.print(i + "\t");
-			for(int j = 0; j < BOARD_LENGTH; j++) {
-				if(this.board[i][j] == State.Blank) {
+			for (int j = 0; j < BOARD_LENGTH; j++) {
+				if (this.board[i][j] == State.Blank) {
 					System.out.print("- ");
-				}else {
+				} else {
 					System.out.print(this.board[i][j] + " ");
 				}
 			}
 			System.out.println();
 		}
 	}
-	
+
+	// !!!!!!!!!!!!!!!!!!!!
+	public boolean checkWin(int x, int y) {
+		return false;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		Board board = new Board();
 		board.initialization();
 		board.show();
+		int[] a = board.getMoveIndex(14);
+		for (int val : a) {
+			System.out.println(val);
+		}
+
 	}
 
 }
--- src/TicTacToe/Console.java
+++ src/TicTacToe/Console.java
@@ -12,26 +12,33 @@ public class Console {
 		board.initialization();
 	}
 	
-	public void play() {
-		System.out.print("please input the next step:");
+	public void play() {		
+		System.out.print("please input the next step(format eg: 6 6):");
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		
 		board.move(x, y);
 		board.show();
-		if(board.checkWin(x, y, State.O) || board.checkWin(x, y, State.X)) {
-			System.out.println("win!!!");
-		}
+//		if(board.checkWin()) {
+//			System.out.println("win!!!");
+//		}
 	}
 	
 	
 	public static void main(String[] args) {
 		Console console = new Console();
 		console.board.show();
-		while(true) {
+		for(int i = 0; i <2; i++) {
 			console.play();
 			
 		}
+		Board newBoard = new Board();
+		newBoard = console.board.deepcopy();
+		newBoard.move(14, 14);
+		newBoard.show();
+		System.out.println("new length" + newBoard.getAvaliableMoveLen());
+		console.board.show();
+		System.out.println("old length" + console.board.getAvaliableMoveLen());
 	}
 	
 }
